38c190995dd90789a48bc1a2d9a658ede1aa4e1c
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2023-02-24 15:56:42 +0200

    add hotels list

--- PlatinumDev.HotelsWebAPI/Program.cs
+++ PlatinumDev.HotelsWebAPI/Program.cs
@@ -1,6 +1,33 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+var _hotels = new List<Hotel>();
 
-app.MapGet("/", () => "Hello World!");
+_hotels.Add(new Hotel()
+{
+    Id = 1,
+    Latitude = 21,
+    Longitude = 32,
+    Name = "Hof argaman"
+});
+
+_hotels.Add(new Hotel()
+{
+    Id = 2,
+    Latitude = 22,
+    Longitude = 32,
+    Name = "Nof hatsafon"
+});
+
+app.MapGet("/hotels", () => _hotels);
 
 app.Run();
+
+
+
+public class Hotel
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int Latitude { get; set; }
+    public int Longitude { get; set; }
+}
