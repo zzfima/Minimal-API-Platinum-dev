996aff50830320af055fbdeaabbd0d1414cee6f9
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2023-02-25 12:46:58 +0200

    add hotel db

--- PlatinumDev.HotelsWebAPI/Program.cs
+++ PlatinumDev.HotelsWebAPI/Program.cs
@@ -1,5 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
+
+//add data context
+builder.Services.AddDbContext<HotelDB>(options =>
+{
+    //add configuration from appsettings.json
+    options.UseSqlite(builder.Configuration.GetConnectionString("Sqlite"));
+});
 var app = builder.Build();
+
+//while development ensure db scheme created while app started
+if (app.Environment.IsDevelopment())
+{
+    using var scope = app.Services.CreateScope();
+    var dbService = scope.ServiceProvider.GetRequiredService<HotelDB>();
+    dbService.Database.EnsureCreated();
+}
+
 var _hotels = new List<Hotel>();
 
 //Get all hotels
@@ -24,3 +40,10 @@ app.MapPut("/hotels", (Hotel h) =>
 });
 
 app.Run();
+
+//db context
+public class HotelDB : DbContext
+{
+    public HotelDB(DbContextOptions<HotelDB> options) : base(options) { }
+    public DbSet<Hotel> Hotels => Set<Hotel>();
+}
